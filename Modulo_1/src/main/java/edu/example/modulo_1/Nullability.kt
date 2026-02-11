eba88e96f3271215563db31a48efe88d6ab860bb
Manuel24 <jmendez28@alumnos.uaq.mx>
2026-02-11 08:31:42 -0600

    Initial commit

--- Modulo_1/src/main/java/edu/example/modulo_1/Nullability.kt
+++ Modulo_1/src/main/java/edu/example/modulo_1/Nullability.kt
@@ -4,7 +4,5 @@ fun main(){
     var marbles: Int? = null
 
     var fishFoodTreats = 6
-    if (fishFoodTreats != null) {
-        fishFoodTreats = fishFoodTreats.dec()
-    }
+    fishFoodTreats = fishFoodTreats?.dec() ?: 0
 }
