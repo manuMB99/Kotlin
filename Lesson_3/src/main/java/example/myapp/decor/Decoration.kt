900c5a8628a7ee6359a5afe43a63e54ba33b6f81
Manuel24 <jmendez28@alumnos.uaq.mx>
2026-03-03 09:53:51 -0600

    Initial commit

--- Lesson_3/src/main/java/example/myapp/decor/Decoration.kt
+++ Lesson_3/src/main/java/example/myapp/decor/Decoration.kt
@@ -4,3 +4,17 @@ data class Decoration(val rocks: String) {
 }
 data class Decoration2(val rocks: String, val wood: String, val diver: String){
 }
+
+class Choice {
+    companion object {
+        var name: String = "lyric"
+        fun showDescription(name:String) = println("My favorite $name")
+    }
+}
+
+fun main() {
+    println(Choice.name)
+    Choice.showDescription("pick")
+    Choice.showDescription("selection")
+}
+
